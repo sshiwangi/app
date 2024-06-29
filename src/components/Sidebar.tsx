11ea4e4b51651ce89d43249e97f1ae15f7a243a4
Evan <mr.evan.gordon@gmail.com>
2024-06-29 14:55:18 +0100

    added avatar profile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,31 +2,36 @@ import Image from 'next/image';
 import React, { ReactNode } from 'react';
 import devlaunch from '../assets/devlaunch.png';
 import { IoMdLogOut } from 'react-icons/io';
-import { RxAvatar } from 'react-icons/rx';
-
+import linkedin from '../assets/LinkedIn.png';
 interface SidebarProps {
   children: ReactNode;
 }
 export default function Sidebar({ children }: SidebarProps) {
-  const someVariabnle = 'some tfef';
   return (
     <div className="flex h-screen bg-gray-900">
-      <div className=" flex-col justify-between w-28 bg-gray-800">
-        <aside className="flex h-[calc(100vh_-_2rem)]  flex-col items-center justify-between  py-6">
+      <div className=" flex-col justify-between  bg-gray-800">
+        <aside className="  flex h-[calc(90vh_-_2rem)]  flex-col items-center justify-between  py-6">
           <span>
             <Image
-              className="rounded-3xl p-4"
+              className="h-28 w-28 mt-8 rounded-3xl p-4"
               objectFit="cover"
               alt="devlaunch"
               src={devlaunch}
             />
           </span>
-          <span>
-            <button>
-              <IoMdLogOut size={44} color="white" className="  " />
-            </button>{' '}
-          </span>
         </aside>
+        <div className="flex  flex-col    items-center justify-between ">
+          <button>
+            <IoMdLogOut size={44} color="white" className="mb-4" />
+          </button>{' '}
+          <span>
+            <Image
+              className=" h-8 w-8 rounded-full mb-4 ring-2 ring-white"
+              src={linkedin}
+              alt="profile"
+            />
+          </span>
+        </div>
       </div>
       <div className=" flex-1 p-4 relative">{children}</div>
     </div>
